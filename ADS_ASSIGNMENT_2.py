88ad02a5d6007cfe0a261423ffdb10f6086373fc
Varala9014 <125889569+Varala9014@users.noreply.github.com>
2023-04-05 23:39:56 +0100

    final update of the fine
    this update include all possible changes and required outcomes

--- ADS_ASSIGNMENT_2.py
+++ ADS_ASSIGNMENT_2.py
@@ -85,3 +85,66 @@ def plotting_data(indicator_name):
 plotting_data(indicator_name="Agriculture, forestry, and fishing, value added (% of GDP)")
 plotting_data(indicator_name="CO2 emissions from gaseous fuel consumption (% of total)")
 plotting_data(indicator_name="Total greenhouse gas emissions (kt of CO2 equivalent)")
+
+
+#storing variable data to find farming effieciency
+collected_data = plotting_data(indicator_name="Population growth (annual %)")
+plotting_data(indicator_name="CO2 emissions (kg per 2015 US$ of GDP)")
+
+#defining the mean function for 
+def mean_stats():
+    """This function plots the graph for the mean of the Global Warming's 
+    Effects on Agriculture, Fishing, and Pollution: A Long-Term Analysis with 
+    the help of
+    pandas statistical function mean()"""
+
+    # mean of the Farming effeciency
+    mean_series = collected_data.mean(numeric_only=True)
+    mean_data = pd.DataFrame({"Years": years, "farming effeciency":
+                              mean_series})
+    mean_data.set_index("Years").plot(
+        rot=0, title="Global Warming's Effects on Agriculture, Fishing, and Pollution: A Long-Term Analysis"
+    )
+
+
+# calling  mean_stats function for lineplot
+mean_stats()
+
+
+"""Defining the function to plot the heatmap with the help of seaborn to create 
+   correlation between the indicator and also show the graphical reprentation
+   of the correlation between the indicator to get the over all overview
+"""
+def plot_corr(country_name):
+    """This function takes the Country Name as argument and cross compare
+    the correlations between different indicators of the Country and plot the heatmap"""
+
+    # filter the dataframe with given country name
+    country_data = main_dataframe[main_dataframe["Country Name"] == country_name]
+    
+    # list of indicators for the country
+    indicator_names = [
+        "Agriculture, forestry, and fishing, value added (% of GDP)",
+        "Population, total",
+        "CO2 emissions (kt)",
+        "Forest area (sq. km)",
+        "Agricultural land (sq. km)",
+        "CO2 emissions from gaseous fuel consumption (% of total)",
+    ]
+
+    # set the index as Indicator Name and filter the dataframe with given indicator names and years
+    country_data_indicator = country_data.set_index("Indicator Name")
+
+    # extract the data for the given years and indicator names and transpose the dataframe
+    extracted_data_t = country_data_indicator.loc[indicator_names, years].transpose()
+    
+
+    # plot the heatmap for the correlation between different indicators
+    plt.title(country_name, fontsize=20)
+    sns.heatmap(extracted_data_t.corr(), linecolor='white',
+                linewidths=0.1, annot=True, cmap="Accent")
+    return extracted_data_t
+
+#calling the correlation funtion
+plot_corr("Austria")
+plot_corr("Belgium")
